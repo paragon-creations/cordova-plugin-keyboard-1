c05fe29802a8c4f1675a1a4896ef70fc99ca4ea4
Oren Agiv <oa@pnc.co.il>
2017-01-21 01:00:27 +0200

    Debugging...

--- src/android/KeyboardPlugin.java
+++ src/android/KeyboardPlugin.java
@@ -31,18 +31,21 @@ public class KeyboardPlugin extends CordovaPlugin {
         
         callback = callbackContext;
         
+        view.setFocusable(true);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        
         if ("register".equals(action)) {
             view.setOnKeyListener(
                 new OnKeyListener() {
                     @Override
-                    //public boolean onKey(View v, int keyCode, KeyEvent event) {
-                    public boolean onKeyUp(int keyCode, KeyEvent event) {
-                        //if (event.getAction() == KeyEvent.ACTION_UP) {
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if (event.getAction() == KeyEvent.ACTION_UP) {
                             //callbackContext.success("KeyCode: "+(String.valueOf(keyCode)));
                             PluginResult result = new PluginResult(PluginResult.Status.OK, "KeyCode: "+String.valueOf(keyCode));
                             result.setKeepCallback(true);
                             callback.sendPluginResult(result);
-                        //}
+                        }
                         return true;
                     };
                 }
