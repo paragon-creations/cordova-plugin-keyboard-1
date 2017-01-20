160f332a6cc94b7ce5fbcbdb8c8cae088da5f197
Oren Agiv <oa@pnc.co.il>
2017-01-20 23:54:08 +0200

    Debugging...

--- src/android/KeyboardPlugin.java
+++ src/android/KeyboardPlugin.java
@@ -12,32 +12,32 @@ public class KeyboardPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	Activity activity = this.cordova.getActivity();
-	InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+    	Activity activity = this.cordova.getActivity();
+    	InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
 
-	View view;
-	try {
-	    view = (View)webView.getClass().getMethod("getView").invoke(webView);
-	}
-	catch (Exception e){
-	    view = (View)webView;
-	}
+    	View view;
+    	try {
+    	    view = (View)webView.getClass().getMethod("getView").invoke(webView);
+    	} catch (Exception e) {
+    	    view = (View)webView;
+    	}
 
-	if ("register".equals(action)) {
-        view.setOnKeyListener(
-            new OnKeyListener() {
-                @Override
-                public boolean onKey(View v, int keyCode, KeyEvent event) {
-                    if (event.getAction() == KeyEvent.ACTION_UP) {
-                        callbackContext.success("KeyCode: "+(String.valueOf(keyCode)));
-                    }
-                    return true;
-                };
-            }
-        );
-	    return true;
-	} else {
-	   callbackContext.error(action + " is not a supported action");
-	      return false;
+    	if ("register".equals(action)) {
+            view.setOnKeyListener(
+                new OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if (event.getAction() == KeyEvent.ACTION_UP) {
+                            callbackContext.success("KeyCode: "+(String.valueOf(keyCode)));
+                        }
+                        return true;
+                    };
+                }
+            );
+    	    return true;
+    	} else {
+            callbackContext.error(action + " is not a supported action");
+            return false;
+        }
     }
 }
