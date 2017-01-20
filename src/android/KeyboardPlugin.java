f6883d2e1bf67ab9fcc9cf0527f8e3e83b045c9a
Oren Agiv <oa@pnc.co.il>
2017-01-21 00:49:35 +0200

    Debugging...

--- src/android/KeyboardPlugin.java
+++ src/android/KeyboardPlugin.java
@@ -1,14 +1,20 @@
-package org.apache.cordova.labs.keyboard;
+package com.otb.cordova.keyboard;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.Context;
-import android.view.inputmethod.InputMethodManager;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.KeyEvent;
-import org.apache.cordova.*;
-import org.json.JSONArray;
-import org.json.JSONException;
+import android.view.*;
 
 public class KeyboardPlugin extends CordovaPlugin {
     private static CallbackContext callback = null;
@@ -23,15 +29,16 @@ public class KeyboardPlugin extends CordovaPlugin {
     	    view = (View)webView;
     	}
         
+        callback = callbackContext;
+        
         if ("register".equals(action)) {
-            callback = callbackContext;
             view.setOnKeyListener(
                 new OnKeyListener() {
                     @Override
                     public boolean onKey(View v, int keyCode, KeyEvent event) {
                         if (event.getAction() == KeyEvent.ACTION_UP) {
                             //callbackContext.success("KeyCode: "+(String.valueOf(keyCode)));
-                            PluginResult result = new PluginResult(PluginResult.Status.OK, "KeyCode: "+(String.valueOf(keyCode)));
+                            PluginResult result = new PluginResult(PluginResult.Status.OK, "KeyCode: "+String.valueOf(keyCode));
                             result.setKeepCallback(true);
                             callback.sendPluginResult(result);
                         }
@@ -41,7 +48,7 @@ public class KeyboardPlugin extends CordovaPlugin {
             );
     	    return true;
     	} else {
-            callbackContext.error(action + " is not a supported action");
+            callback.error(action + " is not a supported action");
             return false;
         }
     }
